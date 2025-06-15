38318121e51f3f5d55f945086bfd8fd560a21e19
Gnomeev <JleoHug2000@gmail.com>
2025-06-15 13:44:30 +0300

    almost

--- Content.Server/SS220/EntityEffects/Effects/VisibilityChangeEffect.cs
+++ Content.Server/SS220/EntityEffects/Effects/VisibilityChangeEffect.cs
@@ -1,3 +1,4 @@
+using Content.Server.SS220.Stealth.TemporalStealth;
 using Content.Shared.EntityEffects;
 using Content.Shared.Stealth;
 using Content.Shared.Stealth.Components;
@@ -10,14 +11,18 @@ public sealed partial class VisibilityChangeEffect : EntityEffect
     [DataField]
     public float VisibilityChange = 0.5f;
 
+    [DataField]
+    public TimeSpan VisibilityDuration = TimeSpan.FromSeconds(1);
+
     public override void Effect(EntityEffectBaseArgs args)
     {
         var stealthSystem = args.EntityManager.System<SharedStealthSystem>();
+        var temporalSystem = args.EntityManager.System<TemporalStealthSystem>();
 
         if (args.EntityManager.TryGetComponent<StealthComponent>(args.TargetEntity, out var stealth))
         {
             var effectVisibility = stealthSystem.GetVisibility(args.TargetEntity, stealth) + VisibilityChange;
-            stealthSystem.SetVisibility(args.TargetEntity, effectVisibility, stealth);
+            temporalSystem.ActivateTemporalStealth(args.TargetEntity, effectVisibility, VisibilityDuration);
         }
     }
     protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
