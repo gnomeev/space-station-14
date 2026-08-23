56ef8e88752db0752af6be1eea20fc96b83f695c
Anri <elyasin.ane@mail.ru>
2026-08-23 22:30:08 +0300

    init-commit (#4532)

--- Content.Server/SS220/Surgery/Systems/SurgerySystem.cs
+++ Content.Server/SS220/Surgery/Systems/SurgerySystem.cs
@@ -12,6 +12,7 @@ namespace Content.Server.SS220.Surgery.Systems;
 public sealed partial class SurgerySystem : SharedSurgerySystem
 {
     [Dependency] private BloodstreamSystem _bloodstream = default!;
+    [Dependency] private IPrototypeManager _prototypeManager = default!;
 
     public override void Initialize()
     {
@@ -29,12 +30,24 @@ public sealed partial class SurgerySystem : SharedSurgerySystem
 
     private void OnSurgeryPatientMove(Entity<SurgeryPatientComponent> entity, ref MoveEvent args)
     {
-        if (entity.Comp.OngoingSurgeries.Count == 0)
+        var countForBleed = 0;
+        foreach (var (key, nodeId) in entity.Comp.OngoingSurgeries)
+        {
+            if (!_prototypeManager.Resolve(key, out var resolvedSurgery))
+                continue;
+
+            if (nodeId == resolvedSurgery.Start)
+                continue;
+
+            countForBleed++;
+        }
+
+        if (countForBleed == 0)
             return;
 
         var distance = MathF.Min((args.NewPosition.Position - args.OldPosition.Position).Length(), 4f);
 
-        _bloodstream.TryModifyBleedAmount(entity.Owner, entity.Comp.OnSurgeryMoveBleed * distance * entity.Comp.OngoingSurgeries.Count);
+        _bloodstream.TryModifyBleedAmount(entity.Owner, entity.Comp.OnSurgeryMoveBleed * distance * countForBleed);
     }
 
     protected override void ProceedToNextStep(Entity<SurgeryPatientComponent> entity, EntityUid user, EntityUid? used, ProtoId<SurgeryGraphPrototype> surgeryGraph, SurgeryGraphEdge chosenEdge)
--- Content.Shared/SS220/Surgery/Components/SurgeryPatientComponent.cs
+++ Content.Shared/SS220/Surgery/Components/SurgeryPatientComponent.cs
@@ -1,6 +1,5 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
-using Content.Shared.Damage;
 using Content.Shared.SS220.Surgery.Graph;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
